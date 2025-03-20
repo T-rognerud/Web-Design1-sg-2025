690d453b73524d7601210be3eb7eb56546c6d248
T-rognerud <Trognerud@outlook.com>
2025-03-20 11:04:34 -0500

    footer

--- components/js/footer.js
+++ components/js/footer.js
@@ -7,23 +7,27 @@ class Footer extends HTMLElement {
     this.innerHTML = `
 
 
-<footer><div class="container my-5">
-
-    <footer class="text-center text-lg-start" style="background-color: #261C33;">
-      <div class="container d-flex justify-content-center py-5">
-        </button>
-        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #D57798;">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar w/ text</a>
+   <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #D57798;">
           <i class="fab fa-youtube"></i>
-        </button>
-        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #D57798;">
-          <i class="fab fa-instagram"></i>
-        </button>
-      </div>
-
-    </footer>
-    
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Navbar text with an inline element
+    </span>
   </div>
-</footer>
+</nav>
 
           `;
   }
